524daa4f5e74c6146d6181fe302ef15cfbd22c14
bukvic6 <bukvic6@gmail.com>
2022-08-15 01:13:33 +0200

    email message when dumbbell arrive

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import time
 import hashlib
+import smtplib
+
+
 from urllib.request import urlopen, Request
 
-url = Request('https://www.blockchain.com/explorer',headers={'User-Agent': 'Mozilla/5.0'})
+
+url = Request('https://www.decathlon.rs/disk-tegovi/7278-8967-teg-od-livenog-gvozda-28-mm.html#/demodelcolor-1042303/demodelsize-2325_kg?queryID=3a0a346b7ab1d5d5584fcdd9d9e5f297&objectID=969885',headers={'User-Agent': 'Mozilla/5.0'})
 
 response = urlopen(url).read()
 
@@ -24,8 +28,27 @@ while True:
         if newHash == curHash:
             continue
     
-        else:
-            print("something shanged")
+        else:            
+            try:
+                to = 'bukvic6@gmail.com'
+                me = 'bukvic6@gmail.com'
+                passv = 'pass'
+                email = smtplib.SMTP("smtp.gmail.com",587)
+                email.ehlo()
+                email.starttls()
+                email.ehlo()
+                email.login(me,passv)
+                header = 'To:' + to + '\n' + 'From: ' + me + '\n' + 'Subject:Decathlon \n'
+                print(header)
+                msg = header + '\n Dumbbell has arrived!!\n\n'
+
+                
+                email.sendmail(to,me,msg)
+                print("done")
+            except Exception as e:
+                print(e)
+            finally:
+                email.quit()
 
             response = urlopen(url).read()
             curHash = hashlib.sha224(response).hexdigest()
@@ -34,3 +57,9 @@ while True:
     
     except Exception as e:
         print("error")
+
+
+
+
+
+
